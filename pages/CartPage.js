1ca4669fba1ce1a2c42e269f2179d15a625db9d1
Joe Varghese <joevarghese53@gmail.com>
2024-09-01 14:38:20 +0530

    cart without login

--- pages/CartPage.js
+++ pages/CartPage.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Cart from '../components/Cart';
+import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const CartPage = () => {
-  return <Cart/>;
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return (
+    <>
+      {userInfo ? (
+        <Cart />
+      ) : (
+        <div className="cart-page-not-logged-in">
+          <h1>Please <Link href="/LoginPage">
+            <span style={{ textDecoration: 'underline' }}>LOGIN</span>
+          </Link> to view your cart</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default CartPage;
