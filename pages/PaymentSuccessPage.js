bac20b4f3d5a0a69ae9306b945b6c8d73793f1b9
Joe Varghese <joevarghese53@gmail.com>
2025-07-09 01:47:24 +0530

    redirect fix

--- pages/PaymentSuccessPage.js
+++ pages/PaymentSuccessPage.js
@@ -17,11 +17,18 @@ const PaymentSuccessPage = () => {
   console.log('orderdetails', orderDetails);
 
   useEffect(() => {
-    if (orderDetails && sessionStorage.getItem('paymentSuccess')) {
-      sessionStorage.removeItem('paymentSuccess');
-      runFireworks();
+    if (sessionStorage.getItem('paymentSuccess')) {
+      // Show the page
+      sessionStorage.removeItem('paymentSuccess'); // Remove so it can't be shown again
     } else {
+      // Redirect if flag is missing
       router.replace('/MyOrders');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (orderDetails) {
+      runFireworks();
     }
   }, [orderDetails]);
 
